d6c8a18e4c443440dcb98af5f42f1502c61a7f05
Viktoras Buivydas <viktorasss00@gmail.com>
2021-08-03 21:38:13 +0300

    fixed board show method parameter from id to uid

--- app/Http/Controllers/Api/BoardController.php
+++ app/Http/Controllers/Api/BoardController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Board\StoreRequest;
 use App\Http\Requests\Board\UpdateRequest;
 use App\Http\Resources\Board\ShowResource;
-use App\Http\Resources\Board\StoreResource;
 use App\Models\Board;
 use App\Models\Game;
 use App\Services\BoardService;
@@ -20,18 +19,18 @@ class BoardController extends Controller
         $board = $game->squares()->get();
 
         return ShowResource::collection($board);
-
     }
 
     public function store(StoreRequest $request)
     {
 
         $game = Game::where('uid', $request->uid)->select('id')->firstOrFail();
-
-
+        $squares = Board::where('game_id', $game->id);
+        $square_count = $squares->count();
+        abort_if($square_count >= 9, 422, "game board already exists");
         Board::insert((new BoardService())->generateBoard($game->id));
 
-        return new StoreResource($request);
+        return ShowResource::collection($squares->get());
     }
 
     public function update(UpdateRequest $request)
--- app/Models/Board.php
+++ app/Models/Board.php
@@ -8,9 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Board extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
     protected $fillable = ['x', 'y', 'isX', 'game_id'];
     protected $casts = [
         'isX' => 'boolean',
     ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
 }
